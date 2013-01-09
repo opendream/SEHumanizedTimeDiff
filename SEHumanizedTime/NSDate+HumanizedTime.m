79b3c72361584588bb73d3c7fc5577f584eeaf95
Joshua Kovach <kovach.jc@gmail.com>
2013-01-09 14:53:29 -0500

    Use localized strings from bundle

--- SEHumanizedTime/NSDate+HumanizedTime.m
+++ SEHumanizedTime/NSDate+HumanizedTime.m
@@ -8,7 +8,14 @@
 
 #import "NSDate+HumanizedTime.h"
 
+static NSString * const SEHumanizedTimeTable = @"Localizable";
+
 @implementation NSDate (HumanizedTime)
+
+- (NSBundle *)_SEHumanizedTimeBundle {
+    NSString* path = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"SEHumanizedTime.bundle"];
+    return [NSBundle bundleWithPath:path];
+}
 
 - (NSString *) stringWithHumanizedTimeDifference {
     
@@ -22,7 +29,7 @@
     int minutesDiff = abs((abs(timeInterval) - ((daysDiff * secondsInADay) + (hoursDiff * 60))) / 60);
     //int secondsDiff = (abs(timeInterval) - ((daysDiff * secondsInADay) + (hoursDiff * 3600) + (minutesDiff * 60)));
     
-    NSString *positivity = [NSString stringWithFormat:@"%@", timeInterval < 0 ? NSLocalizedString(@"AgoKey", @""):NSLocalizedString(@"LaterKey", @"")];
+    NSString *positivity = [NSString stringWithFormat:@"%@", timeInterval < 0 ? NSLocalizedStringFromTableInBundle(@"AgoKey", SEHumanizedTimeTable, [self _SEHumanizedTimeBundle], nil):NSLocalizedStringFromTableInBundle(@"LaterKey", SEHumanizedTimeTable, [self _SEHumanizedTimeBundle], nil)];
     
     
     //Some languages don't need whitespeces between words.
@@ -35,28 +42,28 @@
     }
     
     if (yearsDiff > 1)
-        return [NSString stringWithFormat:@"%d%@%@%@%@", yearsDiff, spaceBetweenWords, NSLocalizedString(@"YearsKey", @""), spaceBetweenWords, positivity];
+        return [NSString stringWithFormat:@"%d%@%@%@%@", yearsDiff, spaceBetweenWords, NSLocalizedStringFromTableInBundle(@"YearsKey", SEHumanizedTimeTable, [self _SEHumanizedTimeBundle], nil), spaceBetweenWords, positivity];
     else if (yearsDiff == 1)
-        return [NSString stringWithFormat:@"%@%@%@", NSLocalizedString(@"YearKey", @""), spaceBetweenWords, positivity];
+        return [NSString stringWithFormat:@"%@%@%@", NSLocalizedStringFromTableInBundle(@"YearKey", SEHumanizedTimeTable, [self _SEHumanizedTimeBundle], nil), spaceBetweenWords, positivity];
     
     if (daysDiff > 0) {
         if (hoursDiff == 0)
-            return [NSString stringWithFormat:@"%d%@%@%@%@", daysDiff, spaceBetweenWords, daysDiff == 1 ? NSLocalizedString(@"DayKey", @""):NSLocalizedString(@"DaysKey", @""), spaceBetweenWords, positivity];
+            return [NSString stringWithFormat:@"%d%@%@%@%@", daysDiff, spaceBetweenWords, daysDiff == 1 ? NSLocalizedStringFromTableInBundle(@"DayKey", SEHumanizedTimeTable, [self _SEHumanizedTimeBundle], nil):NSLocalizedStringFromTableInBundle(@"DaysKey", SEHumanizedTimeTable, [self _SEHumanizedTimeBundle], nil), spaceBetweenWords, positivity];
         else
-            return [NSString stringWithFormat:@"%d%@%@%@%d%@%@%@%@", daysDiff, spaceBetweenWords, daysDiff == 1 ? NSLocalizedString(@"DayKey", @""):NSLocalizedString(@"DaysKey", @""), spaceBetweenWords, hoursDiff, spaceBetweenWords, NSLocalizedString(@"HoursKey", @""), spaceBetweenWords, positivity];
+            return [NSString stringWithFormat:@"%d%@%@%@%d%@%@%@%@", daysDiff, spaceBetweenWords, daysDiff == 1 ? NSLocalizedStringFromTableInBundle(@"DayKey", SEHumanizedTimeTable, [self _SEHumanizedTimeBundle], nil):NSLocalizedStringFromTableInBundle(@"DaysKey", SEHumanizedTimeTable, [self _SEHumanizedTimeBundle], nil), spaceBetweenWords, hoursDiff, spaceBetweenWords, NSLocalizedStringFromTableInBundle(@"HoursKey", SEHumanizedTimeTable, [self _SEHumanizedTimeBundle], nil), spaceBetweenWords, positivity];
     }
     else {
         if (hoursDiff == 0) {
             if (minutesDiff == 0)
-                return [NSString stringWithFormat:@"%@%@%@", NSLocalizedString(@"SecondKey", @""), spaceBetweenWords, positivity];
+                return [NSString stringWithFormat:@"%@%@%@", NSLocalizedStringFromTableInBundle(@"SecondKey", SEHumanizedTimeTable, [self _SEHumanizedTimeBundle], nil), spaceBetweenWords, positivity];
             else 
-                return [NSString stringWithFormat:@"%d%@%@%@%@", minutesDiff, spaceBetweenWords, minutesDiff == 1 ? NSLocalizedString(@"MinuteKey", @""):NSLocalizedString(@"MinutesKey", @""), spaceBetweenWords, positivity];
+                return [NSString stringWithFormat:@"%d%@%@%@%@", minutesDiff, spaceBetweenWords, minutesDiff == 1 ? NSLocalizedStringFromTableInBundle(@"MinuteKey", SEHumanizedTimeTable, [self _SEHumanizedTimeBundle], nil):NSLocalizedStringFromTableInBundle(@"MinutesKey", SEHumanizedTimeTable, [self _SEHumanizedTimeBundle], nil), spaceBetweenWords, positivity];
         }
         else {
             if (hoursDiff == 1)
-                return [NSString stringWithFormat:@"%@%@%@%@%@", NSLocalizedString(@"AboutKey", @""), spaceBetweenWords, NSLocalizedString(@"HourKey", @""), spaceBetweenWords, positivity];
+                return [NSString stringWithFormat:@"%@%@%@%@%@", NSLocalizedStringFromTableInBundle(@"AboutKey", SEHumanizedTimeTable, [self _SEHumanizedTimeBundle], nil), spaceBetweenWords, NSLocalizedStringFromTableInBundle(@"HourKey", SEHumanizedTimeTable, [self _SEHumanizedTimeBundle], nil), spaceBetweenWords, positivity];
             else
-                return [NSString stringWithFormat:@"%d%@%@%@%@", hoursDiff, spaceBetweenWords, NSLocalizedString(@"HoursKey", @""), spaceBetweenWords, positivity];
+                return [NSString stringWithFormat:@"%d%@%@%@%@", hoursDiff, spaceBetweenWords, NSLocalizedStringFromTableInBundle(@"HoursKey", SEHumanizedTimeTable, [self _SEHumanizedTimeBundle], nil), spaceBetweenWords, positivity];
         }
     }
 }
